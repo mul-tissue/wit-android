f23c671abfe14ece2b569e02fca3ea6311f5f2b6
heeung <skdi6031@aidot.ai>
2026-01-27 08:37:38 +0900

    modify: Android, Compose 플러그인

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -14,3 +14,20 @@ kotlin {
 dependencies {
     compileOnly(libs.bundles.plugins)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = libs.plugins.com.multissue.wit.application.get().pluginId
+            implementationClass = "com.multissue.convention.plugins.AndroidApplicationPlugin"
+        }
+        register("androidApplicationCompose") {
+            id = libs.plugins.com.multissue.wit.application.get().pluginId
+            implementationClass = "com.multissue.convention.plugins.AndroidApplicationComposePlugin"
+        }
+        register("androidLibraryCompose") {
+            id = libs.plugins.com.multissue.wit.application.get().pluginId
+            implementationClass = "com.multissue.convention.plugins.AndroidLibraryComposePlugin"
+        }
+    }
+}
--- build-logic/convention/src/main/java/com/multissue/convention/extensions/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/multissue/convention/extensions/KotlinAndroid.kt
@@ -64,8 +64,8 @@ internal fun Project.configureKotlinJvm() {
     extensions.configure<JavaPluginExtension> {
         // Up to Java 11 APIs are available through desugaring
         // https://developer.android.com/studio/write/java11-minimal-support-table
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     configureKotlin<KotlinJvmProjectExtension>()
@@ -85,7 +85,7 @@ private inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() =
         is KotlinJvmProjectExtension -> compilerOptions
         else -> TODO("Unsupported project extension $this ${T::class}")
     }.apply {
-        jvmTarget = JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_17
         allWarningsAsErrors = warningsAsErrors
         freeCompilerArgs.add(
             // Enable experimental coroutines APIs, including Flow
