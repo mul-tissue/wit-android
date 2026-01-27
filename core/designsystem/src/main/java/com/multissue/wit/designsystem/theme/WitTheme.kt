8887da0fe4f2a16fb16d32826db8ecb89f02e1e7
Joonhoo <joonhoo0123@gmail.com>
2026-01-27 21:35:08 +0900

    [ feat ]: Theme Colors 추가

--- core/designsystem/src/main/java/com/multissue/wit/designsystem/theme/WitTheme.kt
+++ core/designsystem/src/main/java/com/multissue/wit/designsystem/theme/WitTheme.kt
@@ -1,21 +1,34 @@
 package com.multissue.wit.designsystem.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
+
+private val LocalWitColors =
+    staticCompositionLocalOf<WitColors> { error("All colors must be Provided!") }
 
 private val LocalWitTypography =
     staticCompositionLocalOf<WitTypography> { error("All typography must be Provided") }
 
 @Composable
 fun WitTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    colors: WitColors = if (darkTheme) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
+    },
     typography: WitTypography = WitTypography.defaultTypography(),
     content: @Composable () -> Unit
 ) {
-
+    val provideColors = remember { colors.copy() }
+    provideColors.update(colors)
     CompositionLocalProvider(
+        LocalWitColors provides colors,
         LocalWitTypography provides typography,
     ) {
         MaterialTheme(content = content)
@@ -23,6 +36,11 @@ fun WitTheme(
 }
 
 object WitTheme {
+    val colors: WitColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalWitColors.current
+
     val typography: WitTypography
         @Composable
         @ReadOnlyComposable
