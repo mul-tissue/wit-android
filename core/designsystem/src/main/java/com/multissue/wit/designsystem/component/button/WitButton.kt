22ffcd99d365b0026d0bd099a4098dc5e9edcb0c
Joonhoo <joonhoo0123@gmail.com>
2026-01-30 00:50:34 +0900

    [ modify ] 버튼 leading, trailing icon 추가 및 color 수정

--- core/designsystem/src/main/java/com/multissue/wit/designsystem/component/button/WitButton.kt
+++ core/designsystem/src/main/java/com/multissue/wit/designsystem/component/button/WitButton.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.multissue.wit.designsystem.R
 import com.multissue.wit.designsystem.theme.WitTheme
 
 @Composable
@@ -23,11 +26,13 @@ fun WitButton(
         containerColor = WitTheme.colors.primaryDark,
         contentColor = WitTheme.colors.buttonText,
         disabledContainerColor = WitTheme.colors.disabledButton,
-        disabledContentColor = WitTheme.colors.disabledText
+        disabledContentColor = WitTheme.colors.background
     ),
     shape: Shape = RoundedCornerShape(12.dp),
     title: String,
     enabled: Boolean = true,
+    leadingIcon: @Composable () -> Unit = {},
+    trailingIcon: @Composable () -> Unit = {},
     onClick: () -> Unit,
 ) {
     Button(
@@ -37,10 +42,12 @@ fun WitButton(
         colors = colors,
         onClick = onClick
     ) {
+        leadingIcon()
         Text(
             text = title,
             style = WitTheme.typography.titleM
         )
+        trailingIcon()
     }
 }
 
@@ -51,6 +58,20 @@ fun PreviewWitButtonPreview() {
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
+            WitButton(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .height(56.dp),
+                onClick = {},
+                title = "Button Title",
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.icon_profile),
+                        contentDescription = "",
+                        tint = WitTheme.colors.buttonText
+                    )
+                }
+            )
             WitButton(
                 modifier = Modifier
                     .wrapContentWidth()
