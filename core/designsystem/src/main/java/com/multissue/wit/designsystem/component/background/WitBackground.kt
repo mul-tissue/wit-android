4ae51d01d0fe718b605dbf31dd432ce173f680f9
Joonhoo <joonhoo0123@gmail.com>
2026-01-30 22:07:35 +0900

    [ modify ] 패키지 위치 변경

--- core/designsystem/src/main/java/com/multissue/wit/designsystem/component/background/WitBackground.kt
+++ core/designsystem/src/main/java/com/multissue/wit/designsystem/component/background/WitBackground.kt
@@ -1,4 +1,4 @@
-package com.multissue.wit.designsystem.background
+package com.multissue.wit.designsystem.component.background
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
--- core/designsystem/src/main/java/com/multissue/wit/designsystem/component/searchbar/WitSearchBar.kt
+++ core/designsystem/src/main/java/com/multissue/wit/designsystem/component/searchbar/WitSearchBar.kt
@@ -1,4 +1,4 @@
-package com.multissue.wit.designsystem.searchbar
+package com.multissue.wit.designsystem.component.searchbar
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
